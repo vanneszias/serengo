7e4570cf0e3e5a003c21ee5beac5ba9856c88a36
Zias van Nes <vanneszias@icloud.com>
2025-09-26 18:47:03 +0200

    feat:basic auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,14 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
-}
+		interface Locals {
+			user: import('$lib/server/auth').SessionValidationResult['user'];
+			session: import('$lib/server/auth').SessionValidationResult['session'];
+		}
+	} // interface Error {}
+	// interface Locals {}
+} // interface PageData {}
+// interface PageState {}
 
+// interface Platform {}
 export {};
