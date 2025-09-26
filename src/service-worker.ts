9ce6f963f11d5f733cfff83dcda4ccdb6e391f23
Zias van Nes <vanneszias@icloud.com>
2025-09-26 16:06:30 +0200

    chore:format

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -20,7 +20,7 @@ const CACHE = `cache-${version}`;
 
 const ASSETS = [
 	...build, // the app itself
-	...files  // everything in `static`
+	...files // everything in `static`
 ];
 
 self.addEventListener('install', (event) => {
