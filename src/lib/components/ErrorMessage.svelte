98d745b7fefe43dfbf291b32cdb33722c008dbc3
Zias van Nes <vanneszias@icloud.com>
2025-09-28 15:44:02 +0200

    use:components and remove storybook integration

---
+++ src/lib/components/ErrorMessage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	interface Props {
+		message?: string;
+		show?: boolean;
+	}
+
+	let { message, show = true }: Props = $props();
+</script>
+
+{#if message && show}
+	<p class="error-message">{message}</p>
+{/if}
+
+<style>
+	.error-message {
+		color: #e53e3e;
+		font-size: 0.875rem;
+		text-align: center;
+		margin-top: 1rem;
+		padding: 0.5rem;
+	}
+</style>
