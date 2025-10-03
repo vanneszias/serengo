fbe0a759b7dcf589247109237b2a87001b5dda4a
Zias van Nes <vanneszias@icloud.com>
2025-10-03 17:04:31 +0200

    fix:typo in redirect uri that broke google oauth

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -4,5 +4,5 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
-	'https://sergeno.ziasvannes.tech/login/google/callback'
+	'https://serengo.ziasvannes.tech/login/google/callback'
 );
